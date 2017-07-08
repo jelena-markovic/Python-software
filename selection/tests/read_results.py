b5da059d6632da407285c1dd1b96ec9ceba73ddb
Jelena Markovic <jelenam@stanford.edu>
2017-07-07 20:45:30 -0700

    commit before switching

--- selection/tests/read_results.py
+++ selection/tests/read_results.py
@@ -3,14 +3,17 @@ import pandas as pd
 import selection.tests.reports as reports
 
 
-path = "/Users/Jelena/high_dim_results/*.pkl"
+path = "/Users/Jelena/high_dim_results/experiment_1/*.pkl"
 allFiles = glob.glob(path)
 
 list_dfs = []
+nfiles = 0
 for _file in allFiles:
+    nfiles = nfiles+1
     df = pd.read_pickle(_file)
     list_dfs.append(df)
 
+print("nfiles", nfiles)
 dfs = pd.concat(list_dfs)
 reports.summarize_all(dfs)
 
